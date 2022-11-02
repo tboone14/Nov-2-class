6edd832799de7114b4e0a85051af3aeb391321ef
tboone14 <98772089+tboone14@users.noreply.github.com>
2022-11-02 15:30:41 -0400

    Add files via upload

---
+++ Nov 2.R
@@ -0,0 +1,11 @@
+#Author: Boone Jr, Date: Nov 2, 2022, Purpose: Chi-square analysis
+
+#Reading the sample data
+
+data_frame <- read.csv("https://goo.gl/j6lRXD")
+
+#Check the data counts
+
+table(data_frame$treatment, data_frame$improvement)
+
+chisq.test(data_frame$treatment, data_frame$improvement, correct=FALSE)
